886b18ca106a37d0477e552f37a900714ba6287d
Jeison Rojas <j_esteban96@hotmail.com>
2023-06-01 17:15:03 -0500

    Realizo pruebas con marcadores de colores, reubico y redimensiono cajas

--- src/ar.js
+++ src/ar.js
@@ -7,50 +7,50 @@ const deviceWidth = window.innerWidth;
 
 const boxPositionsDesktop = [
     {
-        position1: '0.0 0.0 1.0'
+        position1: '0.0 0.0 0.8'
     },
     {
-        position1: '-1.0 0.0 1.0',
-        position2: '1.0 0.0 1.0'
+        position1: '-0.5 0.0 0.8',
+        position2: '0.5 0.0 0.8'
     },
     {
-        position1: '-2 0.0 1.0',
-        position2: '0.0 0.0 1.0',
-        position3: '2 0.0 1.0'
+        position1: '-1 0.0 0.8',
+        position2: '0.0 0.0 0.8',
+        position3: '1 0.0 0.8'
     },
     {
-        position1: '-3.0 0.0 1.0',
-        position2: '-1.0 0.0 1.0',
-        position3: '1.0 0.0 1.0',
-        position4: '3.0 0.0 1.0'
+        position1: '-1.5 0.0 0.8',
+        position2: '-0.5 0.0 0.8',
+        position3: '0.5 0.0 0.8',
+        position4: '1.5 0.0 0.8'
     }
 ];
 
 const boxPositionsMobile = [
     {
-        position1: '0.0 0.0 0.4'
+        position1: '0.0 0.0 0.8'
     },
     {
-        position1: '-0.5 0.0 0.4',
-        position2: '0.5 0.0 0.4'
+        position1: '-0.5 0.0 0.8',
+        position2: '0.5 0.0 0.8'
     },
     {
-        position1: '-2 0.0 0.4',
-        position2: '0.0 0.0 0.4',
-        position3: '2 0.0 0.4'
+        position1: '-1 0.0 0.8',
+        position2: '0.0 0.0 0.8',
+        position3: '1 0.0 0.8'
     },
     {
-        position1: '-3.0 0.0 0.4',
-        position2: '-1.0 0.0 0.4',
-        position3: '1.0 0.0 0.4',
-        position4: '3.0 0.0 0.4'
+        position1: '-1.5 0.0 0.8',
+        position2: '-0.5 0.0 0.8',
+        position3: '0.5 0.0 0.8',
+        position4: '1.5 0.0 0.8'
     }
 ];
 
 const boxDimensions = {
-    depth: '0.25',
-    height: '0.25',
-    width: '0.25'
+    depth: '0.4',
+    height: '0.4',
+    width: '0.4'
 };
 
 const boxImagesPath = '../assets/images/';
@@ -94,8 +94,9 @@ const createEntity = (content) => {
     entity.setAttribute('position', '0.0 0.0 -0.5');
     entity.setAttribute('rotation', '0 0 0');
     if (deviceWidth < 500) {
+        // ancho, frente, alto
         // entity.setAttribute('scale', `3 2 1.5`);
-        entity.setAttribute('scale', `0.4 0.5 0.2`);
+        entity.setAttribute('scale', `0.8 0.6 0.3`);
     } else {
         // entity.setAttribute('scale', `3 3 3`);
         entity.setAttribute('scale', `0.7 0.8 0.5`);
